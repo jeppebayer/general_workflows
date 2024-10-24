c8d83583a6f620652ef240e98e89e9c970a76264
jeppebayer <jeppebayer@gmail.com>
2024-10-24 16:34:16 +0200

    24/10-2024

--- population_genetics/migration_sim/workflow_source/scripts/plotting_curves_script.r
+++ population_genetics/migration_sim/workflow_source/scripts/plotting_curves_script.r
@@ -1,9 +1,21 @@
 #!/usr/bin/env Rscript
 
-# workaround before inputs are ready, and if input is unable to be collected and passed (due to length of argument)
+library(scales)
+library(viridis)
+
+args_are <- commandArgs(trailingOnly = TRUE)
+
+
+path_to_search = args_are[1]
+    # path_to_search = "/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/2dSFS/"
+pattern_is = args_are[2]
+    # pattern_is = "bestlikelyhoods"
+name_append = args_are[3]
+    # name_append = "EntNic_all_dat"
 
 #list_of_files <- list.files(pattern="bestlhoods" , path="/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/2dSFS/2dSFS_EntNic_aaRJ_C225_vs_EntNic_aeRoe-C36_1_vs_2/", recursive = TRUE, full.names = TRUE)
-list_of_files <- list.files(pattern="bestlikelyhoods" , path="/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/2dSFS/", recursive = TRUE, full.names = TRUE)
+# workaround before inputs are ready, and if input is unable to be collected and passed (due to length of argument)
+list_of_files <- list.files(pattern = pattern_is , path = path_to_search, recursive = TRUE, full.names = TRUE)
 NROW(list_of_files)
 # remove pops!
 # double check, they should already be removed in the pipeline. Better safe than sorry.
@@ -38,9 +50,15 @@ nrow(df_sort)
 # remove pairs with split time below 7000
 df_sort <- df_sort[which(df_sort$TDIV > 7000),]
 nrow(df_sort)
+# removes 5-6000 pairs
+#[1] 40078
+#> # remove pairs with split time below 7000
+#> df_sort <- df_sort[which(df_sort$TDIV > 7000),]
+#> nrow(df_sort)
+#[1] 34403
 
-
-png(file = "/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/median_migration_curve_EntNic.png", res = 800, units = "in", height = 7, width = 7)
+# migration curve median
+png(file = paste0("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/median_migration_curve_", name_append, ".png"), res = 800, units = "in", height = 7, width = 7)
 mean_frame <- aggregate(df_sort, list(df_sort$migration_divide), FUN = mean, na.rm = T)
 median_frame <- aggregate(df_sort, list(df_sort$migration_divide), FUN = median, rm.na = T)
 plot(mean_frame$Group.1, mean_frame$mig_rec, type = "p", ylim = c(min(c(mean_frame$mig_rec, mean_frame$mig_anc)), .0001), 
@@ -53,8 +71,8 @@ legend("topright", legend=c("Mean", "Median"), col = "black", pch = c(1,2), bty 
 #plot(mean_frame$Group.1, mean_frame$mig_rec, type = "p", ylim = c(min(c(mean_frame$mig_rec, mean_frame$mig_anc)), max(c(mean_frame$mig_rec, mean_frame$mig_anc))))
 #points(mean_frame$Group.1, mean_frame$mig_anc, col = "red", ylim = c(min(c(mean_frame$mig_rec, mean_frame$mig_anc)), max(c(mean_frame$mig_rec, mean_frame$mig_anc))))
 dev.off()
-
-png(file = "/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/median_migration_curve_EntNic_zoom.png", res = 800, units = "in", height = 7, width = 7)
+# migration curve median zoomed
+png(file = paste0("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/median_migration_curve_zoom_", name_append, ".png"), res = 800, units = "in", height = 7, width = 7)
 mean_frame <- aggregate(df_sort, list(df_sort$migration_divide), FUN = mean, rm.na = T)
 median_frame <- aggregate(df_sort, list(df_sort$migration_divide), FUN = median, rm.na = T)
 plot(mean_frame$Group.1, mean_frame$mig_rec, type = "p", ylim = c(min(c(mean_frame$mig_rec, mean_frame$mig_anc)), .00002), 
@@ -68,7 +86,62 @@ legend("topright", legend=c("Mean", "Median"), col = "black", pch = c(1,2), bty 
 #points(mean_frame$Group.1, mean_frame$mig_anc, col = "red", ylim = c(min(c(mean_frame$mig_rec, mean_frame$mig_anc)), max(c(mean_frame$mig_rec, mean_frame$mig_anc))))
 dev.off()
 
+# indv median curves zoomed
+png(file = paste0("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/migration_curves_all_zoom_", name_append, ".png"), res = 800, units = "in", height = 7, width = 7)
+for( pair in unique(sort(df_sort$pop_pair))){
+    if(unique(sort(df_sort$pop_pair))[1] == pair ){
+        datis <- df_sort[which(df_sort$pop_pair == pair),]
+        plot(datis$migration_divide, datis$mig_rec, type = "l", ylim = c(min(c(df_sort$mig_rec, df_sort$mig_anc)), .00002), 
+            ylab = "Gene flow rate", xlab = "Migration divide (Generations)", col = alpha("grey", .7))
+        lines(datis$migration_divide, datis$mig_anc, col = alpha("red", .7), ylim = c(min(c(df_sort$mig_rec, df_sort$mig_anc)), .00002))
+    }else{
+        datis <- df_sort[which(df_sort$pop_pair == pair),]
+        lines(datis$migration_divide, datis$mig_rec, col = alpha("grey", .7), ylim = c(min(c(df_sort$mig_rec, df_sort$mig_anc)), .00002))
+        lines(datis$migration_divide, datis$mig_anc, col = alpha("red", .7), ylim = c(min(c(df_sort$mig_rec, df_sort$mig_anc)), .00002))
+    }
+}
+legend("topright", legend=c("Ancestral migration", "Recent migration"), col = c("red", "black"), pch = 1, bty = "n")
+dev.off()
 
+#try scaling then (z transform)
+scaled_recent = unlist(with(df_sort, tapply(X = mig_rec, INDEX = pop_pair, FUN = scale)))
+scaled_ancestral = unlist(with(df_sort, tapply(X = mig_anc, INDEX = pop_pair, FUN = scale)))
+df_sort_scale <- cbind(df_sort, scaled_recent, scaled_ancestral)
+png(file = paste0("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/migration_curves_all_zoom_scaled_", name_append, ".png"), res = 800, units = "in", height = 7, width = 7)
+for( pair in unique(sort(df_sort_scale$pop_pair))){
+    if(unique(sort(df_sort_scale$pop_pair))[1] == pair ){
+        datis <- df_sort_scale[which(df_sort_scale$pop_pair == pair),]
+        plot(datis$migration_divide, datis$scaled_recent, type = "l", 
+            ylab = "Gene flow rate", xlab = "Migration divide (Generations)", col = alpha("grey", .7))
+        lines(datis$migration_divide, datis$scaled_ancestral, col = alpha("red", .7))
+    }else{
+        datis <- df_sort_scale[which(df_sort_scale$pop_pair == pair),]
+        lines(datis$migration_divide, datis$scaled_recent, col = alpha("grey", .7))
+        lines(datis$migration_divide, datis$scaled_ancestral, col = alpha("red", .7))
+    }
+}
+mean_frame <- aggregate(df_sort_scale, list(df_sort_scale$migration_divide), FUN = mean, rm.na = T)
+median_frame <- aggregate(df_sort_scale, list(df_sort_scale$migration_divide), FUN = median, rm.na = T)
+lines(mean_frame$Group.1, mean_frame$scaled_recent, col = alpha("black", .7))
+lines(mean_frame$Group.1, mean_frame$scaled_ancestral,, col = alpha("orange", .7))
+points(median_frame$Group.1, median_frame$scaled_recent, col = alpha("black", .7))
+points(median_frame$Group.1, median_frame$scaled_ancestral,, col = alpha("orange", .7))
+legend("topright", legend=c("Ancestral migration", "Recent migration"), col = c("red", "black"), pch = 1, bty = "n")
+dev.off()
+# ylim = c(min(c(df_sort_scale$scaled_recent, df_sort_scale$scaled_ancestral)), 11)
+
+png(file = paste0("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/migration_curves_all_zoom_scaled_means_", name_append, ".png"), res = 800, units = "in", height = 7, width = 7)
+mean_frame <- aggregate(df_sort_scale, list(df_sort_scale$migration_divide), FUN = mean, rm.na = T)
+median_frame <- aggregate(df_sort_scale, list(df_sort_scale$migration_divide), FUN = median, rm.na = T)
+ylims_are <- c(min(c(mean_frame$scaled_ancestral, mean_frame$scaled_recent, median_frame$scaled_ancestral, median_frame$scaled_recent)), max(c(mean_frame$scaled_ancestral, mean_frame$scaled_recent, median_frame$scaled_ancestral, median_frame$scaled_recent)))
+plot(mean_frame$Group.1, mean_frame$scaled_recent, type = "l", main = "Scaled gene flow rates",
+            ylab = "Gene flow rate", xlab = "Migration divide (Generations)", ylim = ylims_are)
+lines(mean_frame$Group.1, mean_frame$scaled_ancestral, col = "red", ylim = ylims_are)
+points(median_frame$Group.1, median_frame$scaled_recent, ylim = ylims_are)
+points(median_frame$Group.1, median_frame$scaled_ancestral, col = "red", ylim = ylims_are)
+legend("bottomright", legend=c("Ancestral migration", "Recent migration"), col = c("red", "black"), pch = 1, bty = "n")
+legend("bottom", legend=c("Mean", "Median"), pch = c(NA, 1), lty = c(1, NA), bty = "n")
+dev.off()
 
 
 # plot mean demography
@@ -126,7 +199,7 @@ ylims = c(min(c(z_trans_dat, mean_traj)), max(c(z_trans_dat, mean_traj)))
 #ylims = c(min(z_trans_dat), max(z_trans_dat))
 library(scales)
 # plot Ne trajectories with both mean of population Medians, and z-transformed trajectories (using scale) 
-png("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/Ne_stairway_trajectory_z_and_mean.png", res = 600, height = 7, width = 7, units = "in")
+png(paste0("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/Ne_stairway_trajectory_z_and_mean", name_append, ".png"), res = 600, height = 7, width = 7, units = "in")
 plot(ne_dat[,1], z_trans_dat[,1], type = 'l', col = "grey", ylim = ylims, ylab = "z-transformed Ne (and blue, Nex 10^5)", xlab = "Thousand years bp")
 for(colu in seq(2,ncol(z_trans_dat))){
     lines(ne_dat[,1], z_trans_dat[,colu], col = "grey", ylim = ylims)
@@ -136,7 +209,7 @@ lines(ne_dat[,1], mean_traj, ylim = ylims, lwd = 5, col = "blue")
 dev.off()
  # plot only z transformed
 ylims = c(min(z_trans_dat), max(z_trans_dat))
-png("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/Ne_stairway_trajectory_z_trans.png", res = 600, height = 7, width = 7, units = "in")
+png(paste0("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/Ne_stairway_trajectory_z_trans", name_append, ".png"), res = 600, height = 7, width = 7, units = "in")
 plot(ne_dat[,1],z_trans_dat[,1], type = 'l', col = alpha("orange", .3), ylim = ylims, ylab = "z-transformed Ne", xlab = "Thousand years bp")
 for(colu in seq(2,ncol(z_trans_dat))){
     lines(ne_dat[,1], z_trans_dat[,colu], col = alpha("orange", .3), ylim = ylims)
@@ -173,7 +246,7 @@ dev.off()
     # Agri, stairway (Ne), migration
 
 col_pan <- rev(plasma(5))
-png(file = "/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/median_migration_Ne_agri.png", res = 800, units = "in", height = 7, width = 7)
+png(file = paste0("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/median_migration_Ne_agri", name_append, ".png"), res = 800, units = "in", height = 7, width = 7)
 par(mfrow = c(3,1), mar = c(1.5,5,0,0), oma = c(4, 0, 1, 1))
 xlimis = c(0,4000)
 xpd_is = FALSE
@@ -236,7 +309,7 @@ dev.off()
 # try to combine in one graph?
     # Agri, stairway (Ne), migration
 col_pan <- rev(inferno(3, begin = .2, end = .8))
-png(file = "/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/median_migration_Ne_agri_ONE.png", res = 800, units = "in", height = 6, width = 10)
+png(file = paste0("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/median_migration_Ne_agri_ONE", name_append,".png"), res = 800, units = "in", height = 6, width = 10)
 par(mfrow = c(1,1), mar = c(1.5,5,0,0), oma = c(6, 4, 1, 1))
 xlimis = c(0,4000);     xpd_is = FALSE;     mgp_is = c(3,1,0) # margin line for ax title, labels and line (3,1,0)
 cex_adj = .9
@@ -247,7 +320,7 @@ plot(data[which(data$Landuse == "cropland"),"Year"], data[which(data$Landuse == 
     ylim = range(pretty(c(0, data[which(data$Landuse == "cropland"),"Km2"]))))
 axis(2, las = 2, line = 0, col = col_pan[1], cex.axis = cex_adj)
 rect( 7000/3, -10, 9000/3, 80, col = "grey90", border = NULL, lwd = 0, xpd = F)
-rect( 100, -10, 600, 80, col = "grey90", border = NULL, lwd = 0, xpd = F)
+rect( 100, -10, 650, 80, col = "grey90", border = NULL, lwd = 0, xpd = F)
 lines(data[which(data$Landuse == "cropland"),"Year"], data[which(data$Landuse == "cropland"),"Km2"], col = col_pan[1], xlim = xlimis, xpd = xpd_is)
 
 # migration
@@ -284,11 +357,12 @@ text(-330, -.78, labels=c("Percentage\n Landcover"), col = col_pan[1], xpd = NA,
 text(-600, -.78, labels=c(expression("Gene flow" ~ (10^-5))), col = col_pan[2], xpd = NA, srt = 90, adj = 0)
 text(-900, -.78, labels=c("Ne\n(z-transformed)"), col = col_pan[3], xpd = NA, srt = 90, adj = 0)
 
-legend("bottom", legend=c( "Ancestral gene flow", "Recent gene flow", "Cropland cover", "Ne trajectory"),
-    col = col_pan[c(2,2,1,3)], lty = 1, pch = c(NA, 19, NA, NA), bty = "n", xpd = NA, inset = -.33, ncol = 2)
+legend(900, y=-.63, legend=c( "Ancestral gene flow", "Recent gene flow", "Cropland cover", "Ne trajectory"),
+    col = col_pan[c(2,2,1,3)], lty = 1, pch = c(NA, 19, NA, NA), bty = "n", xpd = NA, ncol = 2,
+    xjust = 0, text.width = 1000)
 dev.off()
 
-
+#inset = -.33
 
 
 
@@ -298,7 +372,7 @@ dev.off()
 
 # trying to dissolve flat migration line
 
-png(file = "/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/median_migration_Ne_recent_zoom.png", res = 800, units = "in", height = 7, width = 7)
+png(file = paste0("/home/anneaa/EcoGenetics/people/anneaa/derived_dat_scripts/neutral_diversity_pipeline/migration_sim/Collembola/Entomobrya_nicoleti/fsc/median_migration_Ne_recent_zoom", name_append, ".png"), res = 800, units = "in", height = 7, width = 7)
 par(mfrow = c(4,1), mar = c(1,5,0,0), oma = c(4, 0, 1, 1))
 xlimis = c(0,4000)
 xpd_is = FALSE
